42fab88810f4b8e765e58b9a1c9364f18b8fd30a
Blowing <1072878103@qq.com>
2018-04-12 09:28:12 +0800

    新建pictureselector

--- app/src/main/java/com/wujie/wutpictureselector/MainActivity.java
+++ app/src/main/java/com/wujie/wutpictureselector/MainActivity.java
@@ -1,13 +1,19 @@
 package com.wujie.wutpictureselector;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView =  findViewById(R.id.image_view);
+
+
     }
 }
---
+++ pictureselector/src/main/java/com/wujie/pictureselector/ddd.java
@@ -0,0 +1,9 @@
+package com.wujie.pictureselector;
+
+/**
+ * Created by wujie on 2018/4/11/011.
+ */
+
+public class ddd {
+
+}
